6debc75c03869088f1a14beb0a935446f63dea43
mevlut <mevlut.yildirim84@erzurum.edu.tr>
2022-06-27 01:58:24 +0300

    added readme

--- README.md
+++ README.md
@@ -1,8 +1,42 @@
-GO + POSTGRESQL DB + HASURA (GRAPHQL) 
-API
-
+GO + POSTGRESQL DB + HASURA (GRAPHQL) API
 
 GO MOD TIDY (PACKAGE INSTALL)
 
 GO RUN cmd/main.go
+
+GO VERSION = go1.18.3 linux/amd64
+
+<hr>
+REQUIRED => # HASURA URL - HASURA SECRET KEY
+<hr>
+
+## Routes
+
+| METHOD | PATH                      | AUTH |
+| ------ | ------------------------- | ---- |
+| GET    | company/:id               | NO   |
+| GET    | user/id                   | NO   |
+| POST   | insert-user               | NO   |
+| POST   | insert-company            | NO   |
+| POST   | insert-partnership        | NO   |
+| POST   | insert-partnership-detail | NO   |
+| DELETE | delete-user               | NO   |
+| DELETE | delete-company            | NO   |
+| DELETE | delete-partnership        | NO   |
+| POST   | all-company               | NO   |
+| POST   | all-user                  | NO   |
+| PUT    | edit-user                 | NO   |
+| PUT    | edit-company              | NO   |
+
+
+## DB TABLE
+
+<br>TABLE</br>
+COMPANIES , USERS , PARTNERSHIP , PARTNERSHIP_DETAILS
+<hr>
+
+<br>RELATIONSHIP</br>
+PARTNERSHIP.company_id = COMPANIES.id
+PARTNERSHIP.user_id = USERS.id
+PARTNERSHIP_DETAILS.partnership_id = PARTNERSHIP.id
 
--- handlers/Handlers.go
+++ handlers/Handlers.go
@@ -8,19 +8,19 @@ import (
 
 func Run() {
 	r := mux.NewRouter()
-	r.HandleFunc("/all-companies/{id}", GetCompaines).Methods(("GET"))
-	r.HandleFunc("/all-users/{id}", getUsers).Methods("GET")
+	r.HandleFunc("/company/{id}", GetCompaines).Methods(("GET"))
+	r.HandleFunc("/user/{id}", getUsers).Methods("GET")
 	r.HandleFunc("/insert-user", insertUser).Methods("POST")
 	r.HandleFunc("/insert-company", insertCompanies).Methods("POST")
 	r.HandleFunc("/insert-partnership", insertPartnership).Methods("POST")
 	r.HandleFunc("/insert-partnership-details", insertPartnershipDetails).Methods("POST")
-	r.HandleFunc("/delete-user", deletedUsers).Methods("POST")
-	r.HandleFunc("/delete-company", deletedCompanies).Methods("POST")
-	r.HandleFunc("/delete-partnership", partnershipCancelled).Methods("POST")
-	r.HandleFunc("/all-userid", getUseridAllPartnership).Methods("POST")
-	r.HandleFunc("/all-companyid", getCompanyIdAllPartner).Methods("POST")
-	r.HandleFunc("/edit-user", editUser).Methods("POST")
-	r.HandleFunc("/edit-company", editCompany).Methods("POST")
+	r.HandleFunc("/delete-user", deletedUsers).Methods("DELETE")
+	r.HandleFunc("/delete-company", deletedCompanies).Methods("DELETE")
+	r.HandleFunc("/delete-partnership", partnershipCancelled).Methods("DELETE")
+	r.HandleFunc("/all-user", getUseridAllPartnership).Methods("POST")
+	r.HandleFunc("/all-company", getCompanyIdAllPartner).Methods("POST")
+	r.HandleFunc("/edit-user", editUser).Methods("PUT")
+	r.HandleFunc("/edit-company", editCompany).Methods("PUT")
 
 	server := &http.Server{Addr: ":4545", Handler: r}
 	server.ListenAndServe()
